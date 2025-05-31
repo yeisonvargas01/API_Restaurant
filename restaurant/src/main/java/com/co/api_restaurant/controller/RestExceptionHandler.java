c660c5b59a7a80a2f987f4e9bc3481f5a1829cca
yeisonvargas01 <yeisonvargass27@gmail.com>
2025-05-30 22:29:13 -0500

    ellow dependencia phenom

---
+++ restaurant/src/main/java/com/co/api_restaurant/controller/RestExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.co.api_restaurant.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, Object>> handleResponseStatusException(ResponseStatusException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now().toString());
+        body.put("status", ex.getStatusCode().value());
+        body.put("error", ex.getStatusCode().toString()); // <-- Línea corregida
+        body.put("message", ex.getReason());
+        return new ResponseEntity<>(body, ex.getStatusCode());
+    }
+}
+
