d96518d96cc4cedc6a209b50c16b35e7a2f0a7b1
yeisonvargas01 <yeisonvargass27@gmail.com>
2025-06-03 14:18:08 -0500

    ellow dependencia phenom carism jpa

--- restaurant/src/main/java/com/co/api_restaurant/service/jpa/JpaOrderRepository.java
+++ restaurant/src/main/java/com/co/api_restaurant/service/jpa/JpaOrderRepository.java
@@ -4,6 +4,6 @@ import com.co.api_restaurant.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaOrderRepository extends JpaRepository<Order, Long> {
-    // Puedes agregar métodos personalizados si los necesitas
+
 }
 
